3c7d7ecbe4e49188e0fb9fa5632001461da6faaa
Raphael <paiva.raphaelt@gmail.com>
2021-10-06 15:56:24 +0200

    refacto: add hot attribut and add withHeat method

--- src/ChocolateMaker.ts
+++ src/ChocolateMaker.ts
@@ -1,15 +1,24 @@
-import { DrinkMaker } from './interfaces/DrinkMakerInterface';
 import Sugar from './Sugar';
+import HotMaker from './HotMaker';
+import { DrinkMaker } from './interfaces/DrinkMakerInterface';
 
 class ChocolateMaker implements DrinkMaker {
   private _sugar: Sugar;
+  private _hot?: HotMaker;
 
-  constructor(sugar: Sugar) {
+  constructor(sugar: Sugar, hot?: HotMaker) {
     this._sugar = sugar;
+    this._hot = hot;
   }
 
   public makeDrink(): string {
-    return `H${this._sugar.checkWouldLikeSugar(this._sugar.getCountSugar())}`;
+    return `H${this._withHeat()}${this._sugar.checkWouldLikeSugar(
+      this._sugar.getCountSugar()
+    )}`;
+  }
+
+  private _withHeat(): string {
+    return this._hot ? this._hot.makeHotDrink() : '';
   }
 }
 
--- src/CoffeeMaker.ts
+++ src/CoffeeMaker.ts
@@ -1,15 +1,24 @@
-import { DrinkMaker } from './interfaces/DrinkMakerInterface';
 import Sugar from './Sugar';
+import HotMaker from './HotMaker';
+import { DrinkMaker } from './interfaces/DrinkMakerInterface';
 
 class CoffeeMaker implements DrinkMaker {
   private _sugar: Sugar;
+  private _hot?: HotMaker;
 
-  constructor(sugar: Sugar) {
+  constructor(sugar: Sugar, hot?: HotMaker) {
     this._sugar = sugar;
+    this._hot = hot;
   }
 
   public makeDrink(): string {
-    return `C${this._sugar.checkWouldLikeSugar(this._sugar.getCountSugar())}`;
+    return `C${this._withHeat()}${this._sugar.checkWouldLikeSugar(
+      this._sugar.getCountSugar()
+    )}`;
+  }
+
+  private _withHeat(): string {
+    return this._hot ? this._hot.makeHotDrink() : '';
   }
 }
 
